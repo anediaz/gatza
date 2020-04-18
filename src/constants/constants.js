b89322fe42f26bcbb19bb2446c2a077dda573026
Ane Diaz De Tuesta <anediaz@gmail.com>
2020-04-18 15:21:07 +0200

    Refactoring of FlickrAPI and constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,28 +1,19 @@
-const url =
-  "https://api.flickr.com/services/rest/" +
-  "?api_key=45cb4d81ed65f5a656f10046b761cc48" +
-  "&extras=url_o%2Curl_m%2Curl_c%2Curl_l&format=json" +
-  "&method=flickr.photosets.getPhotos&photoset_id=72157670317433475&user_id=114481456%40N05";
-
-export const FLICKR = {
-  url2: "https://api.flickr.com/services/rest/",
-  url: url,
-  api_key: "45cb4d81ed65f5a656f10046b761cc48",
-  user_id: "114481456@N05"
-};
-
+const API_KEY = "45cb4d81ed65f5a656f10046b761cc48";
+const USER_ID = "114481456@N05";
+export const fetchUrl = (photosetId, extras) =>
+  `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&extras=${extras}&format=json&nojsoncallback=true&method=flickr.photosets.getPhotos&photoset_id=${photosetId}&user_id=${USER_ID}`;
 export const EXTRAS = {
   small320: {
     url: "url_n",
     width: "width_n",
-    height: "height_n"
+    height: "height_n",
   },
   original: { url: "url_o", width: "width_o", height: "height_o" },
   medium500: { url: "url_m", width: "width_m", height: "height_m" },
   medium800: { url: "url_c", width: "width_c", height: "height_c" },
-  large1024: { url: "url_l", width: "width_l", height: "height_l" }
+  large1024: { url: "url_l", width: "width_l", height: "height_l" },
 };
 
 export const ERROR_TYPES = {
-  flickrLoading: "Photo loading from Flickr"
+  flickrLoading: "Photo loading from Flickr",
 };
--- src/services/FlickrAPI.js
+++ src/services/FlickrAPI.js
@@ -1,15 +1,12 @@
-import { FLICKR } from "../constants/constants.js";
-
-const getUrl = (photoSetId, sizes) =>
-  `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${FLICKR.api_key}&photoset_id=${photoSetId}&extras=${sizes}&format=json&nojsoncallback=true`;
+import { fetchUrl } from "../constants/constants.js";
 
 async function getPhotos(photoSetId, size) {
-  const response = await fetch(getUrl(photoSetId, size));
+  const response = await fetch(fetchUrl(photoSetId, size));
   return response && response.ok
     ? (await response.json()).photoset.photo
     : { Error: `Error while reading photoset=${photoSetId}` };
 }
 
 export default {
-  getPhotos
+  getPhotos,
 };

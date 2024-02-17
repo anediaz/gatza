d84688c177906d502f2d5e1e75d038100c6e0ccb
Ane Diaz de Tuesta <ane.diazdetuesta@datadoghq.com>
2024-02-17 14:28:15 +0100

    Fix the use of secrets in FlickAPI

--- src/services/FlickrAPI.ts
+++ src/services/FlickrAPI.ts
@@ -2,7 +2,7 @@ import { FLICKR } from './secret.constants';
 
 export const getPhotos = async (photoSetId: string, sizes: string[]) => {
   const sizesParam = sizes.join(',');
-  const getPhotosUrl = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${FLICKR.apiKey}&extras=${sizesParam}&format=json&nojsoncallback=true&method=flickr.photosets.getPhotos&photoset_id=${photoSetId}&user_id=${FLICKR.userId}`;
+  const getPhotosUrl = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${FLICKR.api_key}&extras=${sizesParam}&format=json&nojsoncallback=true&method=flickr.photosets.getPhotos&photoset_id=${photoSetId}&user_id=${FLICKR.user_id}`;
   const response = await fetch(getPhotosUrl);
   return response && response.ok
     ? (await response.json()).photoset.photo

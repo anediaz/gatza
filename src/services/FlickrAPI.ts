dc8126d3d23f443fda757a94767688ebe863fdff
Ane Diaz de Tuesta <ane.diazdetuesta@datadoghq.com>
2024-02-17 14:26:19 +0100

    Refactor constants to separate secret in another file

--- src/services/FlickrAPI.ts
+++ src/services/FlickrAPI.ts
@@ -1,12 +1,10 @@
-import { fetchUrl } from './constants';
+import { FLICKR } from './secret.constants';
 
-async function getPhotos(photoSetId:string, size:string) {
-  const response = await fetch(fetchUrl(photoSetId, size));
+export const getPhotos = async (photoSetId: string, sizes: string[]) => {
+  const sizesParam = sizes.join(',');
+  const getPhotosUrl = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${FLICKR.apiKey}&extras=${sizesParam}&format=json&nojsoncallback=true&method=flickr.photosets.getPhotos&photoset_id=${photoSetId}&user_id=${FLICKR.userId}`;
+  const response = await fetch(getPhotosUrl);
   return response && response.ok
     ? (await response.json()).photoset.photo
-    : { Error: `Error while reading photoset=${photoSetId}` };
-}
-
-export default {
-  getPhotos,
+    : `Error while reading photoset=${photoSetId}`;
 };
--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,22 +1,18 @@
-const API_KEY = '45cb4d81ed65f5a656f10046b761cc48';
-const USER_ID = '114481456@N05';
-export const fetchUrl = (photosetId:string, extras:string) => `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&extras=${extras}&format=json&nojsoncallback=true&method=flickr.photosets.getPhotos&photoset_id=${photosetId}&user_id=${USER_ID}`;
+export enum SizeKeys {
+  small = '_n',
+  original = '_o',
+  medium500 = '_m',
+  medium800 = '_c',
+  large='_l'
+}
 
-export const EXTRAS = {
-  small320: {
-    url: 'url_n',
-    width: 'width_n',
-    height: 'height_n',
+export const CONFIGURATIONS = [
+  { minWidth: 1024, cols: 7, margin: 5 },
+  {
+    minWidth: 480, maxWidth: 1023, cols: 7, margin: 1,
   },
-  original: { url: 'url_o', width: 'width_o', height: 'height_o' },
-  medium500: { url: 'url_m', width: 'width_m', height: 'height_m' },
-  medium800: { url: 'url_c', width: 'width_c', height: 'height_c' },
-  large1024: { url: 'url_l', width: 'width_l', height: 'height_l' },
-};
-
-export const ERROR_TYPES = {
-  flickrLoading: 'Photo loading from Flickr',
-};
+  { maxWidth: 479, cols: 4, margin: 1 },
+];
 
 // Google Analytics
 export const googleTrackId = 'UA-179302321-2';
@@ -36,3 +32,10 @@ export const LOCALES = [
   'CA' as const,
 ];
 export type LocaleType = ValueOf<typeof LOCALES>;
+export const LANGUAGES:Record<LocaleType, string> = {
+  EU: 'euskara',
+  ES: 'castellano',
+  EN: 'english',
+  FR: 'français',
+  CA: 'català',
+};
